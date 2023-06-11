67a1146bdcf003d52318dd7ac4bd9254ca1b82cd
John Law <8260377+Wal8800@users.noreply.github.com>
2023-06-12 10:47:14 +1200

    Add pipeline argument parsing

--- imagr/scripts/pipeline.sh
+++ imagr/scripts/pipeline.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
 set -e
+
+while getopts ":o:d:" opt; do
+  case $opt in
+    o) DATA_DIR="$OPTARG"
+    ;;
+    d) OUTPUT_DIR="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    exit 1
+    ;;
+  esac
+
+  case $OPTARG in
+    -*) echo "Option $opt needs a valid argument"
+    exit 1
+    ;;
+  esac
+done
+
+if [ -z "$DATA_DIR" ]
+then
+    echo "-d can't be empty, need to provide data directory"
+    exit 1
+fi
+
+if [ -z "$OUTPUT_DIR" ]
+then
+    echo "-o can't be empty, need to provide output directory"
+    exit 1
+fi
