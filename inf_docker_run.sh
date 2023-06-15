4d45d3619ea7d5fc42cddf9bb2493a74c8be6aab
walter <walter.wang@imagr.co>
2023-06-15 15:57:58 +1200

    create inf script

--- inf_docker_run.sh
+++ inf_docker_run.sh
@@ -1,4 +1,6 @@
 docker run -it --privileged -v /dev/bus/usb:/dev/bus/usb \
 -v $PWD:/home/tensorflow/models \
+-v $PWD/data_imagr:/data \
+-v $PWD/results:/results \
 -w /home/tensorflow/models \
 edgetpu_inf bash
