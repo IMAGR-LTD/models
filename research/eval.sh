70348be4e690883fb73bb4775841f07a8995f4ef
walter <walter.wang@imagr.co>
2023-06-15 13:37:13 +1200

    modify the eval script

--- research/eval.sh
+++ research/eval.sh
@@ -1,6 +1,6 @@
 MODEL="run1"
 MODEL_DIR=/trained_model/$MODEL
-python3 research/object_detection/model_main.py \
+python3 object_detection/model_main.py \
   --model_dir=$MODEL_DIR \
   --pipeline_config_path=$MODEL_DIR/pipeline.config \
   --run_once \
