fb9ee9a649dddfdf2dd909fbdea03d70ed7864d3
walter <walter.wang@imagr.co>
2023-06-15 13:38:28 +1200

    create a bash script to run docker for eval

---
+++ run_eval_docker.sh
@@ -0,0 +1,4 @@
+docker run --gpus device=3 -it -v $PWD:/home/tensorflow/models \
+-v $PWD/data_imagr:/data \
+-v $PWD/models_imagr:/trained_model \
+-w /home/tensorflow/models/research australia-southeast1-docker.pkg.dev/ml-shared-c-c41d/ml/object_detection_tf1:585776b  bash
